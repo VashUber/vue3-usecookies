0371c50c8effb36a6e14a9d3c7000ddc5f182a02
VashUber <mrjarokvk@gmail.com>
2022-09-02 17:36:52 +0300

    feature: updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,32 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
 
-export default defineConfig({
-  root: resolve(__dirname, "playground"),
-  plugins: [vue()],
+export default defineConfig(({ mode }) => {
+  return {
+    root: resolve(__dirname, mode === "production" ? "" : "playground"),
+    plugins: [
+      vue(),
+      dts({
+        entryRoot: resolve(__dirname, "src"),
+        outputDir: resolve(__dirname, "dist"),
+      }),
+    ],
+    build: {
+      lib: {
+        entry: resolve(__dirname, "src/index.ts"),
+        name: "index",
+        fileName: (format) => `index.${format}.js`,
+      },
+      rollupOptions: {
+        external: ["vue"],
+        output: {
+          globals: {
+            vue: "Vue",
+          },
+        },
+      },
+    },
+  };
 });
